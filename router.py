74b55cd63d51bd1f1a4e36766c65ed40d4b2d46b
pedrohenriquemop <madeirapedrohenrique1112@gmail.com>
2025-05-16 19:00:14 -0300

    add message and clicommand classes

--- router.py
+++ router.py
@@ -5,30 +5,71 @@ import json
 import socket
 
 
+class Utils:
+    @staticmethod
+    def json_as_str(data: object | str):
+        if isinstance(data, str):
+            return data
+
+        return json.dumps(data)
+
+
+class Message:
+    def __init__(self, source: str, destination: str, type: str):
+        self.source = source
+        self.destination = destination
+        self.type = type
+
+
+class DataMessage(Message):
+    def __init__(self, source: str, destination: str, payload: object | str):
+        super().__init__(source, destination, "data")
+        self.payload = Utils.json_as_str(payload)
+
+
+class UpdateMessage(Message):
+    def __init__(self, source: str, destination: str, distances: dict):
+        super().__init__(source, destination, "update")
+        self.distances = distances
+
+
+class TraceMessage(Message):
+    def __init__(self, source: str, destination: str, routers: list[str]):
+        super().__init__(source, destination, "trace")
+        self.routers = routers
+
+
 ACCEPTED_COMMANDS = {"add": "<ip> <weight>", "del": "<ip>", "trace": "<ip>"}
 
 
-def parse_command(command: str):
-    if not command:
-        raise Exception("Empty command")
-    type, *args = command.split(" ")
+class CLICommand:
+    def __init__(self, command: str):
+        self.type, *self.args = self.__parse_command(command)
 
-    args_format = ACCEPTED_COMMANDS.get(type)
+    def __parse_command(self, command: str):
+        if not command:
+            raise Exception("Empty command")
+        type, *args = command.split(" ")
 
-    if not args_format:
-        raise Exception(
-            f"Invalid command type. Available commands: {", ".join(ACCEPTED_COMMANDS.keys())}"
-        )
+        args_format = ACCEPTED_COMMANDS.get(type)
 
-    expected_args_len = len(args_format.split(" "))
+        if not args_format:
+            raise Exception(
+                f"Invalid command type. Available commands: {", ".join(ACCEPTED_COMMANDS.keys())}"
+            )
 
-    if expected_args_len != len(args):
-        raise Exception(f"Usage: {args_format}")
+        expected_args_len = len(args_format.split(" "))
 
-    pattern = r"<(\w+)>"
-    arg_names = re.findall(pattern, args_format)
+        if expected_args_len != len(args):
+            raise Exception(f"Usage: {args_format}")
 
-    return {"type": type, **dict(zip(arg_names, args))}
+        pattern = r"<(\w+)>"
+        arg_names = re.findall(pattern, args_format)
+
+        return (type, *dict(zip(arg_names, args)))
+
+    def __str__(self):
+        return str(self.__dict__)
 
 
 def terminate_program():
@@ -45,7 +86,7 @@ def main():
                 break
 
             try:
-                parsed_command = parse_command(command)
+                parsed_command = CLICommand(command)
 
                 print(parsed_command)
             except Exception as e:
